704696bc7663dd78800113290d9c77fb0f51ac27
Miles Rosenberg <milesrosenberg@gmail.com>
2020-05-11 20:30:44 -0400

    Folded genes Milk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const convert = require('convert-units')
-
+const foldedGenes = "425416110003716118";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -15,50 +15,100 @@ client.on('message', msg => {
 
 client.on('message', message => {
     if(!message.author.bot) {
-        //LB to KG
         let finalMessage = "";
-        if(message.content.toLowerCase().includes('lb')) {
-            let i = 0;
-            let x;
-            let s = 0;
-            let TempMessage = message.content.toLowerCase();
-            while(i >= 0) {
-                x = ""
-                i = TempMessage.indexOf('lb', s);
-                s = i + 1;
-                i--;
-                while(TempMessage[i] == ' ') 
+        if(message.author.id != foldedGenes) {
+            //LB to KG
+            if(message.content.toLowerCase().includes('lb')) {
+                let i = 0;
+                let x;
+                let s = 0;
+                let TempMessage = message.content.toLowerCase();
+                while(i >= 0) {
+                    x = ""
+                    i = TempMessage.indexOf('lb', s);
+                    s = i + 1;
                     i--;
-                while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
-                    x += TempMessage[i];
-                    i--;
+                    while(TempMessage[i] == ' ') 
+                        i--;
+                    while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
+                        x += TempMessage[i];
+                        i--;
+                    }
+                    x = x.split("").reverse().join("");
+                    if(x.length > 0 && !isNaN(x)) {
+                        finalMessage += x + "lb = " + convert(parseInt(x)).from('lb').to('kg').toFixed(0) + "kg\n";
+                    }
                 }
-                x = x.split("").reverse().join("");
-                if(x.length > 0 && !isNaN(x)) {
-                    finalMessage += x + "lb = " + convert(parseInt(x)).from('lb').to('kg').toFixed(0) + "kg\n";
+            }
+            //KG to LB
+            if(message.content.toLowerCase().includes('kg')) {
+                let i = 0;
+                let x;
+                let s = 0;
+                let TempMessage = message.content.toLowerCase();
+                while(i >= 0) {
+                    x = ""
+                    i = TempMessage.indexOf('kg', s);
+                    s = i + 1;
+                    i--;
+                    while(TempMessage[i] == ' ') 
+                        i--;
+                    while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
+                        x += TempMessage[i];
+                        i--;
+                    }
+                    x = x.split("").reverse().join("");
+                    if(x.length > 0 && !isNaN(x)) {
+                        finalMessage += x + "kg = " + convert(parseInt(x)).from('kg').to('lb').toFixed(0) + "lb\n";
+                    }
                 }
             }
         }
-        //KG to LB
-        if(message.content.toLowerCase().includes('kg')) {
-            let i = 0;
-            let x;
-            let s = 0;
-            let TempMessage = message.content.toLowerCase();
-            while(i >= 0) {
-                x = ""
-                i = TempMessage.indexOf('kg', s);
-                s = i + 1;
-                i--;
-                while(TempMessage[i] == ' ') 
+        else {
+            //LB to KG
+            if(message.content.toLowerCase().includes('lb')) {
+                let i = 0;
+                let x;
+                let s = 0;
+                let TempMessage = message.content.toLowerCase();
+                while(i >= 0) {
+                    x = ""
+                    i = TempMessage.indexOf('lb', s);
+                    s = i + 1;
                     i--;
-                while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
-                    x += TempMessage[i];
-                    i--;
+                    while(TempMessage[i] == ' ') 
+                        i--;
+                    while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
+                        x += TempMessage[i];
+                        i--;
+                    }
+                    x = x.split("").reverse().join("");
+                    if(x.length > 0 && !isNaN(x)) {
+                        finalMessage += x + "lb = " + (parseInt(x)/8.4).toFixed(2) + " Gallons of Milk\n";
+                    }
                 }
-                x = x.split("").reverse().join("");
-                if(x.length > 0 && !isNaN(x)) {
-                    finalMessage += x + "kg = " + convert(parseInt(x)).from('kg').to('lb').toFixed(0) + "lb\n";
+            }
+            //KG to LB
+            if(message.content.toLowerCase().includes('kg')) {
+                let i = 0;
+                let x;
+                let s = 0;
+                let TempMessage = message.content.toLowerCase();
+                while(i >= 0) {
+                    x = ""
+                    i = TempMessage.indexOf('kg', s);
+                    s = i + 1;
+                    i--;
+                    while(TempMessage[i] == ' ') 
+                        i--;
+                    while(i >= 0 && TempMessage[i] != ' ' && !isNaN(TempMessage[i])) {
+                        x += TempMessage[i];
+                        i--;
+                    }
+                    x = x.split("").reverse().join("");
+                    if(x.length > 0 && !isNaN(x)) {
+                        finalMessage += x + "kg = " + (parseInt(x)/4.682).toFixed(2) + " Gallons of Milk\n";
+                    }
                 }
             }
         }
@@ -66,4 +116,4 @@ client.on('message', message => {
             message.channel.send(finalMessage);
     }
 });    
-client.login('NjYyNTIyODA1MTIzOTQwMzUz.Xrnhbg.I66aJasWckZjIbvsB9qSCYFAJsw');
+client.login('NjY0NzE0MjA0MTQ1NjQ3NjE2.XrnrgA.npoS9bWQXO81fOD_D9Xl3u6vmuc');
